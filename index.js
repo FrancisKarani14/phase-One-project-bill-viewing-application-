cce07ebe6dac8f890df5b8bea8d87894549f3ee0
Francis <franciskarani15@gmail.com>
2025-06-24 12:32:51 +0300

    creates a form for user details

---
+++ index.js
@@ -0,0 +1,14 @@
+// create a form for login and sign up
+function signupForm() {
+   return`
+   <div class="users"style="background:rgba(255, 255, 255, 0.788);height:auto;padding:10px;margin:10px;box-shadow:5px 5px 9px rgba(0,0,0,0.08);">
+   <form>
+   <h1 class="formH1">
+   <input type="text" id="name" placeholder="Enter your name">
+   <input type="email" id="email" placeholder="Enter your email">
+   <input type="password" id="password" placeholder="Enter your name">
+   <input type="text" id="country" placeholder="Enter your country's name">
+   <input type="text" id="county" placeholder="Enter your county's name">
+   `
+    
+}
