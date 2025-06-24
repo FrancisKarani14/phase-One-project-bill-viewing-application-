01ee5a5e1f2286fa162a5bc869112db5aa2e2b30
Francis <franciskarani15@gmail.com>
2025-06-24 22:17:27 +0300

    wrires the login logic

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function signupForm() {
         <input type="text" id="county" placeholder="Enter your county's name">
         <button id="loginBtn" type="button">Login</button>
         <button id="signupBtn" type="button">Sign up</button>
-        <p class="hasAccount"></p>
+        // <p class="hasnoAccount"></p>
       </form>
     </div>
   `;
@@ -32,6 +32,9 @@ joinBtn.addEventListener("click", () => {
   const userCounty = document.querySelector("#county");
   const signupBtn = document.querySelector("#signupBtn");
   const formH1 = document.querySelector(".formH1");
+    const emailInput = document.querySelector("#email");
+  const passwordInput = document.querySelector("#password");
+   const loginBtn = document.querySelector("#loginBtn");
     // hides the inputs and changes the button to login
   userName.style.display = "none";
   userCountry.style.display = "none";
@@ -41,8 +44,39 @@ joinBtn.addEventListener("click", () => {
   intro.style.display = "none"
 //   changes the h1 from (sigup to citizen bills, to login to citizen bills)
   formH1.textContent = "Login to Citizen Bills";
+  
+//   handles the login attempt
+loginBtn.addEventListener("click", async()=>{
+    const email = emailInput.value.trim()
+    const password = passwordInput.value.trim()
+
+    // validation to ensure the email and the password are keyed
+    if (!email || !password) {
+        alert("Please enter both email and password")
+        return;
+        
+    }
+    // fetch request to make sure the user details match what is there in the server
+    try{
+        const res = await fetch(`http://localhost:3000/users?email=${email}&passord=${password}`)
+        // converts the server response into json object and saves it into the users variable
+        // it returns an array if a user matches the datails and an empty array if the user matches no details
+        const users = await res.json()
+        // validation to check whether the user matches details
+        if (users.length > 0) {
+            alert("Login successful")
+            
+        }else {
+            alert("You dont have an account, sign up")
+            signupForm()
+            
+        }
+        
+    }catch (error) {
+      console.error("Error during login:", error);
+      alert("Something went wrong. Please try again.");
+    }
+})
 });
 
-function register() {
-    
-}
+
